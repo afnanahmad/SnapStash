a404912c2259b12dcdee4679f00dbfeb587f806e
Afnan Ahmad <afnanahmaddd@gmail.com>
2024-10-19 22:30:17 -0400

    Initial Commit

---
+++ SnapStash/SnapStashApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnapStashApp.swift
+//  SnapStash
+//
+//  Created by Afnan Ahmad on 2024-10-19.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnapStashApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
